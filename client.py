73619d690a5876d8b6ba8075e7b994b39771b114
Matt Garber <mgarber93@gmail.com>
2021-03-14 22:44:04 -0400

    Ignore pycache

--- client.py
+++ client.py
@@ -4,19 +4,17 @@ from common import host, port
 
 def client():
     """Create a client"""
-    client_socket = socket.socket()
-    client_socket.connect((host, port))
-    print(f"Connected to: ({host}, {port})")
+    with socket.socket() as client_socket:
+        client_socket.connect((host, port))
+        print(f"Connected to: ({host}, {port})")
 
-    message = input('Enter message to send...\n>')
-
-    while message != '/q':
-        client_socket.send(message.encode())
-        data = client_socket.recv(1024).decode()
-        print(data)
         message = input('Enter message to send...\n>')
 
-    client_socket.close()
+        while message != '/q':
+            client_socket.send(message.encode())
+            data = client_socket.recv(1024).decode()
+            print(data)
+            message = input('Enter message to send...\n>')
 
 
 if __name__ == '__main__':
