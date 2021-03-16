08b967f0010bd2436e1fc444f4451dd63d2c26dc
Matt Garber <mgarber93@gmail.com>
2021-03-15 21:17:35 -0400

    Implement proof of concept

--- client.py
+++ client.py
@@ -2,19 +2,27 @@ import socket
 from common import host, port
 
 
+def read_and_print(connection):
+    """Read from connection and print as utf-8"""
+    data = connection.recv(1024).decode()
+    print(data)
+
+
+PROMPT = 'Guess a letter...\n>'
+
+
 def client():
     """Create a client"""
     with socket.socket() as client_socket:
         client_socket.connect((host, port))
         print(f"Connected to: ({host}, {port})")
-
-        message = input('Enter message to send...\n>')
+        read_and_print(client_socket)
+        message = input(PROMPT)
 
         while message != '/q':
             client_socket.send(message.encode())
-            data = client_socket.recv(1024).decode()
-            print(data)
-            message = input('Enter message to send...\n>')
+            read_and_print(client_socket)
+            message = input(PROMPT)
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,28 +1,46 @@
-import socket
+import random
+from socket import socket, SOL_SOCKET, SO_REUSEADDR
+from hangman import HangManGame
 from common import host, port
 
 MAX_CONNECTIONS = 1
 
+with open("words.txt") as words_file:
+    content = words_file.read().splitlines()
+
 
 def server():
     """Create a server"""
-
-    with socket.socket() as server_socket:
+    with socket() as server_socket:
+        server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         server_socket.bind((host, port))
         print(f"Listening on: {host} on port: {port}")
         server_socket.listen(MAX_CONNECTIONS)
         client_connection, address = server_socket.accept()
         print(f"Connection by: ({client_connection}, {address})")
 
-        while True:
-            print("Waiting for message...")
+        random_word = random.choice(content)
+        print(f"starting hangman game with: {random_word}")
+        game = HangManGame(random_word)
+
+        while not game.is_game_over():
+            client_connection.send(game.prompt().encode())
             data = client_connection.recv(1024).decode()
             if not data:
                 break
-            print(str(data))
+            letter = str(data)
+            if not game.is_valid_guess(letter):
+                client_connection.send(f'Invalid letter {letter}!'.encode())
+            else:
+                game.guess(letter)
+                client_connection.send(game.prompt().encode())
 
-            message = input('Enter message to send...\n>')
-            client_connection.send(message.encode())
+        if game.has_found_all_letters():
+            final_message = 'You win!'
+        else:
+            final_message = 'You lose!'
+
+        client_connection.send(final_message.encode())
 
 
 if __name__ == '__main__':
